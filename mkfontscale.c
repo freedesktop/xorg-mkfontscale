0af8ff60b3f560339e6f3f9bcf7ac5bc99d8187f
Egbert Eich <eich@suse.de>
2004-03-05 13:41:16 +0000

    23. Merged with XFree86 4.4.0. Added changes that went into infected files.     Reverted darwin/bundle/**/Credits.rtf to XFree86 versions to avoid     future conflicts on ASCII but not humal readable files. (There should     probably be separate CreditsXorg.rtf files) (Egbert Eich).

--- mkfontscale.c
+++ mkfontscale.c
@@ -20,7 +20,7 @@
   THE SOFTWARE.
 */
 /* $XdotOrg: xc/programs/mkfontscale/mkfontscale.c,v 1.1.4.2 2003/12/06 13:24:29 kaleb Exp $ */
-/* $XFree86: xc/programs/mkfontscale/mkfontscale.c,v 1.22 2003/12/19 02:05:39 dawes Exp $ */
+/* $XFree86: xc/programs/mkfontscale/mkfontscale.c,v 1.21 2003/12/10 02:58:07 dawes Exp $ */
 
 #include <stdio.h>
 #include <stdlib.h>
